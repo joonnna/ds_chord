81fa6f6a75f27ac37cea3d699b1df28318826e8d
Jon Foss Mikalsen <jmi021@post.uit.no>
2016-09-14 10:19:21 +0200

    Started on launch

--- launch/launch.go
+++ launch/launch.go
@@ -1,8 +1,46 @@
 package launch
 
 import (
-
-
+	"os/exec"
+	"log"
 )
 
 
+
+func parseArgs() {
+
+
+}
+
+func getNodeList() {
+	nodeListCmd = "sh rocks_list_random_hosts.sh"
+	cmd := exec.command(nodeListCmd)
+
+	output, err := cmd.Run().Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+func initClient() {
+
+
+}
+
+func initNode(ip string) {
+
+}
+
+
+func initNameserver() {
+
+
+}
+
+
+func main () {
+	nodeList := getNodeList()
+	fmt.Println(nodeList)
+
+}
